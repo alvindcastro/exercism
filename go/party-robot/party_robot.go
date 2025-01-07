2fc792dcc1a6531ecab16efd70b8e434087cc86d
Alvin De Castro <alvin.dcastro@gmail.com>
2025-01-07 15:40:24 -0800

    Implement logic

--- go/party-robot/party_robot.go
+++ go/party-robot/party_robot.go
@@ -1,16 +1,39 @@
 package partyrobot
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
 // Welcome greets a person by name.
 func Welcome(name string) string {
-	panic("Please implement the Welcome function")
+	return "Welcome to my party, " + name + "!"
 }
 
 // HappyBirthday wishes happy birthday to the birthday person and exclaims their age.
 func HappyBirthday(name string, age int) string {
-	panic("Please implement the HappyBirthday function")
+	return "Happy birthday " + name + "! You are now " + strconv.Itoa(age) + " years old!"
 }
 
 // AssignTable assigns a table to each guest.
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	panic("Please implement the AssignTable function")
+	assignedTable := "You have been assigned to table " + fmt.Sprintf("%03d", table)
+	//formatFloat := strconv.FormatFloat(distance, 'f', -1, 64)
+	formatFloat := toFixed(distance, 1)
+
+	return "Welcome to my party, " + name + "!" + "\n" +
+		assignedTable +
+		". Your table is " + direction + ", exactly " + fmt.Sprintf("%.1f", formatFloat) + " meters from here." + "\n" +
+		"You will be sitting next to " + neighbor + "."
+
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
 }
