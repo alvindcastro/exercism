b166275ea88143c84ec15b1a234b2cafb49afc83
Alvin De Castro <alvin.dcastro@gmail.com>
2025-01-08 12:33:33 -0800

    Implement 2 items

--- go/card-tricks/card_tricks.go
+++ go/card-tricks/card_tricks.go
@@ -2,13 +2,17 @@ package cards
 
 // FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
-	panic("Please implement the FavoriteCards function")
+	return []int{2, 6, 9}
 }
 
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
-	panic("Please implement the GetItem function")
+	if index > len(slice) || index < 0 {
+		return -1
+	} else {
+		return slice[index]
+	}
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
